7e3eb6d1adca019a2671c52835ad71e11ea4f449
alibkm95 <ali.bkm95@gmail.com>
2024-05-12 15:05:06 +0330

    init: project initialized and primary files and folders created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './assets/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src={logo} className='w-36' alt="" />
+    </>
+  )
+}
+
+export default App
