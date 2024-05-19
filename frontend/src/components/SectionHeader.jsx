26d56e98141651e6e8af328a360875958addcb57
alibkm95 <ali.bkm95@gmail.com>
2024-05-19 15:30:52 +0330

    SectionHeader components layout updated

--- frontend/src/components/SectionHeader.jsx
+++ frontend/src/components/SectionHeader.jsx
@@ -5,7 +5,7 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 
 const SectionHeader = ({ title, icon, showLink = false, linkPath }) => {
   return (
-    <div className="bg-base-200 p-6 rounded-ss-xl rounded-se-xl flex flex-col items-center justify-center gap-4 border-b border-b-gray-700 sm:flex-row sm:justify-between">
+    <div className="bg-base-200 p-6 rounded-ss-xl rounded-se-xl flex flex-col items-center justify-center gap-4 border-b border-b-gray-700 sm:flex-row sm:justify-between lg:px-16">
       <h4 className="font-bold text-3xl flex items-center gap-2">
         {icon}
         {title}
