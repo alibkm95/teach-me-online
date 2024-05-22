ac0ee8268d66c59464d29a14b67fe64d6ad4fc82
alibkm95 <ali.bkm95@gmail.com>
2024-05-22 12:23:54 +0330

    login page and its components created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import LoginForm from '../components/LoginForm';
+
+import { FaSignInAlt } from "react-icons/fa";
+
+import loginImg from '../assets/login.svg'
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className='w-full max-w-6xl mx-auto my-6 p-2'>
+      <div className="bg-base-200 p-4 rounded-box md:p-8">
+        <div className="flex items-center justify-center gap-2 font-bold mb-6 text-2xl md:justify-start lg:text-3xl">
+          <FaSignInAlt className='text-amber-500' />
+          <h5>Login</h5>
+        </div>
+        <div className="flex flex-col gap-4 md:flex-row md:items-center">
+          <div className="flex-1 bg-base-300 rounded-box">
+            <img className='w-full block' src={loginImg} alt="" />
+          </div>
+          <div className="flex-1 flex flex-col gap-4 md:py-6">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
