202d4617bbdd08fabb76c38926ec695efa54411c
alibkm95 <ali.bkm95@gmail.com>
2024-05-29 15:18:57 +0330

    payment page created

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import paymentImg from '../assets/payment.svg'
+
 const Payment = () => {
   return (
-    <div>Payment</div>
+    <section className="w-full max-w-6xl min-h-screen mx-auto my-6 p-2">
+      <div className="grid grid-cols-1 gap-4 place-items-center p-6 bg-base-200 rounded-box md:grid-cols-2">
+        <div className="w-full bg-base-300 rounded-box">
+          <img className='w-full block aspect-square' src={paymentImg} alt="payment image" />
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-3xl font-bold text-center">
+            The fake payment is ongoing, please wait ...
+          </p>
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      </div>
+    </section>
   )
 }
 
